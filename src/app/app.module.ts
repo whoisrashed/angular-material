cab359edc0164b73b7f5c4d1abb26e5eb4b07463
Md Rashedul Islam <rashedul.islam@selise.ch>
2023-11-05 22:30:10 +0600

    date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { MaterialDatepickerComponent } from './material-datepicker/material-datepicker.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +18,8 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatCardModule,
     MatButtonModule,
     MatTooltipModule,
-    MatIconModule
+    MatIconModule,
+    MaterialDatepickerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-datepicker/material-datepicker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-material-datepicker',
+  standalone: true,
+  templateUrl: './material-datepicker.component.html',
+  styleUrls: ['./material-datepicker.component.scss'],
+  imports: [CommonModule],
+})
+export class MaterialDatepickerComponent {
+}
