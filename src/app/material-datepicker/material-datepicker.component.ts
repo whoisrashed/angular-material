9fe8721d76857858ba4204d40066dc395676bc37
Md Rashedul Islam <rashedul.islam@selise.ch>
2023-11-06 19:24:54 +0600

    date picker

--- src/app/material-datepicker/material-datepicker.component.ts
+++ src/app/material-datepicker/material-datepicker.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-material-datepicker',
   standalone: true,
   templateUrl: './material-datepicker.component.html',
   styleUrls: ['./material-datepicker.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
 })
 export class MaterialDatepickerComponent {
+
+  today = new Date();
+  maxDate = new Date('2024-04-03');
 }
